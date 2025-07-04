1eb5add261fde35d6756bf076de47edf14cb7f9b
Fabricio Nathan <fabricionathan749@gmail.com>
2025-07-04 19:38:27 -0300

    integracao com back-end iniciada

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -2,9 +2,30 @@ import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
 
 
 export default function Login() {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleLogin = async (e: React.FormEvent) => {
+        e.preventDefault();
+
+        try {
+            const res = await axios.post("", {
+                username,
+                password
+            });
+            alert("Login autorizado" + res.data);
+        } catch (err) {
+            alert("Login nao autorizado");
+            console.error(err);
+        }
+    };
+
     return (
         <div className="border-2 min-h-screen flex flex-row bg-[linear-gradient(to_right,white_50%,#0b2e66_50%)]">
             <div className="w-1/2 flex items-center justify-center">
@@ -17,7 +38,7 @@ export default function Login() {
             </div>
             <div className="w-1/2 flex items-center justify-end pr-20">
 
-                <form className="flex flex-col  text-amber-400 gap-4 w-[300px]">
+                <form onSubmit={handleLogin} className="flex flex-col  text-amber-400 gap-4 w-[300px]">
                     <h1 className="text-white text-2xl flex items-center justify-center">Login</h1>
                     <p className="text-white">Se você já possui uma conta, pode fazer login com seu username e senha.</p>
                     <Label htmlFor="username">Username</Label>
@@ -27,6 +48,8 @@ export default function Login() {
                         name="username"
                         id="username"
                         placeholder="Digite seu username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
                         required
                     />
                     <Label htmlFor="password">Senha</Label>
@@ -36,6 +59,8 @@ export default function Login() {
                         name="password"
                         id="password"
                         placeholder="Digite sua senha"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
                         required
                     />
                     <div>
