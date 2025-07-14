bc69f4609bd9e394ce43433a19baa948db126aa0
Fabricio Nathan <fabricionathan749@gmail.com>
2025-07-14 20:45:15 -0300

    Pagina home quase concluida

--- app/cadastro/page.tsx
+++ app/cadastro/page.tsx
@@ -1,51 +1,68 @@
+'use client'
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Select, SelectContent, SelectTrigger, SelectValue, SelectItem } from "@/components/ui/select";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
+
+
+
 
 export default function Cadastro() {
-    return (
-        <div className="min-h-screen flex items-center justify-center bg-blue-950">
-            <div className="">
-                <h1 className=" text-center mb-6 font-bold text-white text-2xl">
-                    Realizar Cadastro
-                </h1>
-                <form className=" text-amber-400 space-y-4">
-                    <Label htmlFor="nome">Nome completo</Label>
-                    <Input className="rounded-full text-white" type="text" name="nome" id="nome" placeholder="Digite seu nome completo" required></Input>
-                    <Label htmlFor="username">Username</Label>
-                    <Input className="rounded-full text-white" type="text" name="username" id="username" placeholder="Digite seu username" required></Input>
-                    <Label htmlFor="email">Email</Label>
-                    <Input className="rounded-full text-white" type="email" name="email" id="email" placeholder="Digite seu email" required></Input>
-                    <Select>
-                        <Label>Cargo</Label>
-                        <SelectTrigger className="w-full rounded-full text-white">
-                            <SelectValue placeholder="Escolha seu cargo"></SelectValue>
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectItem value="suporte">Suporte</SelectItem>
-                            <SelectItem value="cordenador">Cordenador</SelectItem>
-                            <SelectItem value="administrador">Administrador</SelectItem>
-                        </SelectContent>
-                    </Select>
-                    <Label htmlFor="password">Senha</Label>
-                    <Input className="rounded-full text-white" type="password" name="password" id="password" placeholder="Digite sua senha" required></Input>
-                    <Label htmlFor="password2">Confirmar Senha</Label>
-                    <Input className="rounded-full text-white" type="password" name="password2" id="password2" placeholder="Confirme sua senha" required></Input>
+    const router = useRouter();
 
-                </form>
-                <div className="flex justify-center pt-8">
-                    <Button type="submit" className="text-black bg-white  hover:bg-amber-400 active:bg-blue-950 px-27 rounded-full cursor-pointer transition-all duration-300 ease-in-out">Cadastrar</Button>
+    const [nome, setNome] = useState("");
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [password2, setPassword2] = useState("");
+
+    const handleCadastro = async (e: React.FormEvent) => {
+        e.preventDefault();
+    }
+        return (
+            <div className="min-h-screen flex items-center justify-center bg-blue-950">
+                <div className="">
+                    <h1 className=" text-center mb-6 font-bold text-white text-2xl">
+                        Realizar Cadastro
+                    </h1>
+                    <form onSubmit={handleCadastro} className=" text-amber-400 space-y-4">
+                        <Label htmlFor="nome">Nome completo</Label>
+                        <Input className="rounded-full text-white" type="text" name="nome" id="nome" placeholder="Digite seu nome completo" value={nome} onChange={(e) => setNome(e.target.value)} required></Input>
+                        <Label htmlFor="username">Username</Label>
+                        <Input className="rounded-full text-white" type="text" name="username" id="username" placeholder="Digite seu username" value={username} onChange={(e) => setUsername(e.target.value)} required></Input>
+                        <Label htmlFor="email">Email</Label>
+                        <Input className="rounded-full text-white" type="email" name="email" id="email" placeholder="Digite seu email" value={email} onChange={(e) => setEmail(e.target.value)} required></Input>
+                        <Select>
+                            <Label>Cargo</Label>
+                            <SelectTrigger className="w-full rounded-full text-white">
+                                <SelectValue placeholder="Escolha seu cargo"></SelectValue>
+                            </SelectTrigger>
+                            <SelectContent>
+                                <SelectItem value="suporte">Suporte</SelectItem>
+                                <SelectItem value="cordenador">Cordenador</SelectItem>
+                                <SelectItem value="administrador">Administrador</SelectItem>
+                            </SelectContent>
+                        </Select>
+                        <Label htmlFor="password">Senha</Label>
+                        <Input className="rounded-full text-white" type="password" name="password" id="password" placeholder="Digite sua senha" value={password} onChange={(e) => setPassword(e.target.value)} required></Input>
+                        <Label htmlFor="password2">Confirmar Senha</Label>
+                        <Input className="rounded-full text-white" type="password" name="password2" id="password2" placeholder="Confirme sua senha" value={password2} onChange={(e) => setPassword2(e.target.value)} required></Input>
+
+                    </form>
+                    <div className="flex justify-center pt-8">
+                        <Button type="submit" className="text-black bg-white  hover:bg-amber-400 active:bg-blue-950 px-27 rounded-full cursor-pointer transition-all duration-300 ease-in-out">Cadastrar</Button>
+                    </div>
+                    <p className="text-white text-center pt-5">
+                        Possui uma conta?{' '}
+                        <a href="/login" className="text-amber-400 hover:underline">
+                            Entre
+                        </a>
+                    </p>
+
                 </div>
-                <p className="text-white text-center pt-5">
-                    Possui uma conta?{' '}
-                    <a href="/login" className="text-amber-400 hover:underline">
-                        Entre
-                    </a>
-                </p>
 
             </div>
-
-        </div>
-    );
-}
+       );
+    }
--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,23 +1,67 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
+import {
+    Sidebar,
+    SidebarContent,
+    SidebarMenuItem,
+    SidebarMenuButton,
+    SidebarMenu,
+    SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
+    SidebarProvider
+} from "@/components/ui/sidebar";
 import { useRouter } from "next/navigation";
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react";
+import Image from "next/image";
+
+const items = [
+    { title: "Home", url: "#", icon: Home },
+    { title: "Teste", url: "#", icon: Inbox },
+    { title: "Teste", url: "#", icon: Calendar },
+    { title: "Teste", url: "#", icon: Search },
+    { title: "Settings", url: "#", icon: Settings },
+];
 
 export default function HomePage() {
     const router = useRouter();
 
     const handleLogout = () => {
-        
-        router.push("/login"); 
+        router.push("/login");
     };
 
     return (
-        <div className="min-h-screen flex flex-col items-center justify-center bg-gradient-to-br from-blue-100 to-blue-300 text-gray-800">
-            <h1 className="text-4xl font-bold mb-4">Bem-vindo à Home!</h1>
-            <p className="mb-6">Você está logado com sucesso.</p>
-            <Button className="bg-red-500 hover:bg-red-600 text-white" onClick={handleLogout}>
-                Sair
-            </Button>
+        <div className="bg-white">
+            <div className="flex m-37 justify-center items-center mr-8">
+                <Image src={"/img/imagem_catolica.png"}  alt="Imagem da faculdade"
+                    width={400}
+                    height={400}></Image>
+            </div>
+            <SidebarProvider>
+                <Sidebar >
+                    <SidebarContent className="bg-blue-950">
+                        <SidebarGroup>
+                            <SidebarGroupLabel className="text-white">Opções</SidebarGroupLabel>
+                            <SidebarGroupContent>
+                                <SidebarMenu className="text-white">
+                                    {items.map((item) => (
+                                        <SidebarMenuItem key={item.title}>
+                                            <SidebarMenuButton asChild>
+                                                <a href={item.url} className="flex items-center gap-2">
+                                                    <item.icon className="w-4 h-4" />
+                                                    <span>{item.title}</span>
+                                                </a>
+                                            </SidebarMenuButton>
+                                        </SidebarMenuItem>
+                                    ))}
+                                </SidebarMenu>
+                            </SidebarGroupContent>
+                        </SidebarGroup>
+                    </SidebarContent>
+                </Sidebar>
+
+            </SidebarProvider>
+
         </div>
     );
 }
